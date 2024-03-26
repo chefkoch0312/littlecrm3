1b8bb100554964944c4ade8d81c4c9dfa26454e5
Kai Dombrowski <kai.dombrowski@web.de>
2024-03-26 14:21:41 +0100

    keine loading animation

--- app/kunden/page.jsx
+++ app/kunden/page.jsx
@@ -4,8 +4,8 @@ import Link from 'next/link'
 let mySearch = "";
 
 async function getData() {
-    await new Promise(resolve => setTimeout(resolve, 1000));
-    // await new Promise(resolve => setTimeout(resolve, 5000));
+    // await new Promise(resolve => setTimeout(resolve, 1000));
+    await new Promise(resolve => setTimeout(resolve, 15000));
     // throw new Error('Simulierter Fehler beim Abrufen der Kunden-Daten');
 
     const res = await fetch('http://localhost:4000/customer/', {
