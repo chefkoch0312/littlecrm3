9d95d88cc5d0a3b68b297ebb5b750619ca50fa23
Kai Dombrowski <kai.dombrowski@web.de>
2024-03-27 09:02:56 +0100

    test

--- app/kunden/kunde/[id]/EditForm.jsx
+++ app/kunden/kunde/[id]/EditForm.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useState, useEffect } from 'react';
 
 async function getData(id) {
     // throw new Error('Simulierter Fehler beim Abrufen der Kunden-Daten');
@@ -12,32 +12,163 @@ async function getData(id) {
     return res.json()
 }
 
-export default async function EditForm(props) {
+export default function EditForm(props) {
     const router = useRouter();
-    const tmpData = await getData(props.id);
 
-    // const [isLoading, setIsLoading] = useState(false);
-    // const [vorname, setVorname] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+    const [tmpData, setTmpData] = useState(null);
+    const [firma, setFirma] = useState('');
+    const [anrede, setAnrede] = useState('');
+    const [vorname, setVorname] = useState('');
+    // const [nachname, setNachname] = useState('');
+    // const [strasse, setStrasse] = useState('');
+    // const [plz, setPLZ] = useState('');
+    // const [ort, setOrt] = useState('');
+    // const [tel, setTel] = useState('');
+    // const [email, setEmail] = useState('');
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setIsLoading(true);
+            try {
+                const data = await getData(props.id);
+                setTmpData(data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        fetchData();
+    }, [props.id]);
+
+    console.log(tmpData);
+    setVorname("test");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setIsLoading(true);
+
+        const post = {
+            firma, anrede, vorname, nachname, strasse, plz, ort, tel, email
+        }
+
+        const res = await fetch('http://localhost:4000/customer/', {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(post)
+        });
+
+        if (res.status === 201) {
+            router.refresh();
+            router.push("/kunden");
+        }
+    }
 
     return (
-        <>
-            <div>
-                <h2>Kunde:</h2>
-                <div>{
+        <form onSubmit={handleSubmit} >
+            <div className="w-80">
+                <div className="columns-4 p-1 w-80 inline-block">
+                    <div className="w-56">Firma:</div>
+                    <div className="w-80"><input
+                        className="input input-green"
+                        required
+                        type="text"
+                        onChange={(e) => setFirma(e.target.value)}
+                        value={firma}
+                    /></div>
+                </div>
+                <div className="columns-4 p-1 w-80 inline-block">
+                    <div className="w-56">Anrede:</div>
+                    <div className="w-80"><input
+                        className="input input-green"
+                        required
+                        type="text"
+                        onChange={(e) => setAnrede(e.target.value)}
+                        value={anrede}
+                    /></div>
+                </div>
+                <div className="columns-4 p-1 w-80 inline-block">
+                    <div className="w-56">Vorname:</div>
+                    <div className="w-80"><input
+                        className="input input-green"
+                        required
+                        type="text"
+                        onChange={(e) => setVorname(e.target.value)}
+                        value={vorname}
+                    /></div>
+                </div>
+                <div className="columns-4 p-1 w-80 inline-block">
+                    <div className="w-56">Nachname:</div>
+                    <div className="w-80"><input
+                        className="input input-green"
+                        required
+                        type="text"
+                        onChange={(e) => setNachname(e.target.value)}
+                        value={nachname}
+                    /></div>
+                </div>
+                {/* <div className="columns-4 p-1 w-80 inline-block">
+                    <div className="w-56">Strasse:</div>
+                    <div className="w-80"><input
+                        className="input input-green"
+                        required
+                        type="text"
+                        onChange={(e) => setStrasse(e.target.value)}
+                        value={strasse}
+                    /></div>
+                </div>
+                <div className="columns-4 p-1 w-80 inline-block">
+                    <div className="w-56">PLZ:</div>
+                    <div className="w-80"><input
+                        className="input input-green"
+                        required
+                        type="text"
+                        onChange={(e) => setPLZ(e.target.value)}
+                        value={plz}
+                    /></div>
+                </div>
+                <div className="columns-4 p-1 w-80 inline-block">
+                    <div className="w-56">Ort:</div>
+                    <div className="w-80"><input
+                        className="input input-green"
+                        required
+                        type="text"
+                        onChange={(e) => setOrt(e.target.value)}
+                        value={ort}
+                    /></div>
+                </div>
+                <div className="columns-4 p-1 w-80 inline-block">
+                    <div className="w-56">Tel:</div>
+                    <div className="w-80"><input
+                        className="input input-green"
+                        required
+                        type="text"
+                        onChange={(e) => setTel(e.target.value)}
+                        value={tel}
+                    /></div>
+                </div>
+                <div className="columns-4 p-1 w-80 inline-block">
+                    <div className="w-56">Email:</div>
+                    <div className="w-80"><input
+                        className="input input-green"
+                        required
+                        type="text"
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
+                    /></div>
+                </div> */}
 
-                    < div className="">
-                        <h3>{tmpData.title}</h3>
-                        <p>{tmpData.vorname}</p>
-                        <p>{tmpData.nachname}</p>
-                        <p>{tmpData.strasse}</p>
-                        <p>{tmpData.plz}</p>
-                        <p>{tmpData.ort}</p>
-                        <p>{tmpData.tel}</p>
-                        <p>{tmpData.email}</p>
+                <div className="columns-4 p-1 w-80 inline-block">
+                    <div className="w-56">
+                        <button className="btn btn-blue" disabled={isLoading} >
+                            {isLoading && <span>Speichern...</span>}
+                            {!isLoading && <span>Hinzufügen</span>}
+                        </button>
                     </div>
-
-                }</div>
-            </div >
-        </>
+                </div>
+            </div>
+        </form >
     )
 }
